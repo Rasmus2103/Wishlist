88bb4ad884e8b54d1b650c0844d3905007d0e899
jonathanFS14 <jona08p8@stud.kea.dk>
2023-03-28 14:53:24 +0200

    oprettet wish og wishlist klasser

--- src/main/java/com/example/wishlist/DTO/WishlistDTO.java
+++ src/main/java/com/example/wishlist/DTO/WishlistDTO.java
@@ -1,4 +1,32 @@
 package com.example.wishlist.DTO;
+import com.example.wishlist.Models.Wish;
+
+import java.util.List;
 
 public class WishlistDTO {
+
+    private String name;
+    private List<Wish> wishes;
+
+    public WishlistDTO(String name, List<Wish> wishes) {
+        this.name = name;
+        this.wishes = wishes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Wish> getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(List<Wish> wishes) {
+        this.wishes = wishes;
+    }
+
 }
--- src/main/java/com/example/wishlist/Models/Wish.java
+++ src/main/java/com/example/wishlist/Models/Wish.java
@@ -1,4 +1,59 @@
 package com.example.wishlist.Models;
 
 public class Wish {
+
+    private String name;
+    private String description;
+    private String url;
+    private String price;
+    private String image;
+
+    public Wish(String name, String description, String url, String price, String image) {
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
 }
